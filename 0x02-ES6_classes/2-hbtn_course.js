26a009816b45a06e8dcea1d0f1e3b9cea10157a2
Thulisile Ngomane <ThuleeN@outlook.com>
2023-06-29 12:30:32 +0000

    added files

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,14 @@
+export default class HolbertonCourse {
+	constructor(name, length, student) {
+	  if (typeof name === 'string') {
+	    this._name = name
+	  } else
+	    throw new TypeError('Name must be a string');
+	  }
+	  if (typeof lenght === 'number') {
+	     this.
+	  }
+
+	  }
+	}
+}
