d79f88bdc0d946f4a687ecdff64083629f348ca3
Thulisile Ngomane <ThuleeN@outlook.com>
2023-10-14 05:54:56 +0200

    js files

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity (arr, city, newGrades) {
+  return arr
+    .filter((obj) => obj.location === city)
+    .map((obj) => {
+      const studentsGrade = newGrades.find((grades) => grade.studentId === obj.id);
+      const grade = studentsGrade === undefinded ? 'N/A' : studentsGrade.grade;
+
+      return { ...obj, grade };
+    });
+}
